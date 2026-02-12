30a7ab13494fb6253e49b12f6a7901cd15982918
Gerle <42682250+Gerloutis@users.noreply.github.com>
2026-02-12 13:36:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from automacao import executar
 import os
 
 app = Flask(__name__)
+
+@app.before_request
+def force_https():
+    if request.headers.get("X-Forwarded-Proto") == "http":
+        return redirect(request.url.replace("http://", "https://"))
 
 @app.route("/")
 def home():
@@ -12,5 +17,3 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
-
-
