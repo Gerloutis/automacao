e762bbbcfa1d23af9d11f42c6859423fecd481e7
Gerle <42682250+Gerloutis@users.noreply.github.com>
2026-02-12 16:53:19 -0300

    Update automacao.py

--- automacao.py
+++ automacao.py
@@ -1,2 +1,30 @@
-def executar():
-    return "Automação executada com sucesso!"
+import os
+import json
+import gspread
+from google.oauth2.service_account import Credentials
+
+def enviar_para_planilha(texto):
+
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    credenciais_json = os.environ.get("GOOGLE_CREDENTIALS")
+
+    credenciais_dict = json.loads(credenciais_json)
+
+    creds = Credentials.from_service_account_info(
+        credenciais_dict,
+        scopes=scope
+    )
+
+    client = gspread.authorize(creds)
+
+    planilha = client.open("NomeDaSuaPlanilha")
+
+    aba = planilha.sheet1
+
+    aba.append_row([texto])
+
+    return "Texto enviado com sucesso!"
