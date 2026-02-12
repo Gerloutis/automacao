f2118c7885ce9ea4cbfc923b1ce490bd3adbf248
Gerle <42682250+Gerloutis@users.noreply.github.com>
2026-02-11 21:32:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from automacao import executar
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
 
